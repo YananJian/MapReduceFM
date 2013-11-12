a775a88a468b313c89dfcfac68e7c91db929c62d
lierdong <donlee1124@gmail.com>
2013-11-11 20:31:19 -0500

    bug fix

--- src/dfs/ClassDownloader.java
+++ src/dfs/ClassDownloader.java
@@ -82,8 +82,8 @@ public class ClassDownloader
   {
     ClassDownloader downloader = null;
     try {
-      String path = args[0];
-      String filename = args[1];
+      String filename = args[0];
+      String path = args[1];
       String registryHost = args[2];
       int registryPort = Integer.parseInt(args[3]);
       downloader = new ClassDownloader(path, filename, registryHost, registryPort);
