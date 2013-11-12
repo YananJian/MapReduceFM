20ac1bb54f6f4bb2d92c168d7ad8bc6f74749195
lierdong <donlee1124@gmail.com>
2013-11-11 19:40:18 -0500

    getAllBlocks fixed

--- src/dfs/NameNodeImpl.java
+++ src/dfs/NameNodeImpl.java
@@ -8,6 +8,7 @@ import java.io.BufferedWriter;
 import java.lang.Thread;
 import java.util.Comparator;
 import java.util.List;
+import java.util.LinkedList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -196,8 +197,14 @@ public class NameNodeImpl implements NameNode
     FileInfo fi = fileInfos.get(filename);
     Map<Integer, List<Integer>> result = new LinkedHashMap<Integer, List<Integer>>();
     List<Integer> blockIds = fi.getBlockIds();
-    for (int blockId : blockIds)
-      result.put(blockId, blockInfos.get(blockId).getDataNodeIds());
+    for (int blockId : blockIds) {
+      List<Integer> aliveIds = new LinkedList<Integer>();
+      List<Integer> dataNodeIds = blockInfos.get(blockId).getDataNodeIds();
+      for (int id : dataNodeIds)
+        if (dataNodeInfos.get(id).isAlive())
+          aliveIds.add(id);
+      result.put(blockId, aliveIds);
+    }
     return result;
   }
 
