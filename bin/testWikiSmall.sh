20d68eea57eea016b73557ba965a0532e649d22f
lierdong <donlee1124@gmail.com>
2013-11-15 22:12:25 -0500

    rm Test and add pagecounts_small

--- bin/testWikiSmall.sh
+++ bin/testWikiSmall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python dfs.py put_text pagecounts-20130601-000000 pagecounts-20130601-000000 3
+python dfs.py put_text pagecounts_small.in pagecounts_small_input
 python dfs.py put_class testmr/WikiMediaFilter\$FilterMapper.class testmr/WikiMediaFilter\$FilterMapper.class 3
 python dfs.py put_class testmr/WikiMediaFilter\$FilterReducer.class testmr/WikiMediaFilter\$FilterReducer.class 3
 python dfs.py put_class testmr/WikiMediaFilter.class testmr/WikiMediaFilter.class 3
-java testmr.WikiMediaFilter pagecounts-20130601-000000 PageCountOutput $1 $2
+java testmr.WikiMediaFilter pagecounts_small_input pagecounts_small_output $1 $2
